5d4d53dcfb6542af842589873e3c5ef92ad33cd4
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-09-30 15:17:59 +0300

    js hw 8-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle'; 
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const localStorageTimeKey = 'videoplayer-current-time';
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayTime, 1000), );
+
+function onPlayTime(evt) {
+    console.log('evt.seconds==', evt.seconds);
+    localStorage.setItem(localStorageTimeKey, evt.seconds);
+};
+player.setCurrentTime(localStorage.getItem(localStorageTimeKey));
+
+//  Для себе, тренувався
+player.on('play',onPlay);
+function onPlay(evt) {
+    console.log('duration clip', (evt.duration/60).toFixed(2), "min");
+    console.log('Play clip', (evt.seconds/60).toFixed(2), "min");
+}
+
+
+
+
+
 
