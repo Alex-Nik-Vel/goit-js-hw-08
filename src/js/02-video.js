663bf9f323a42619726a34e8f2bceedc332ba3bd
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-10-02 11:11:13 +0300

    js 8.2 bug fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,28 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const localStorageTimeKey = 'videoplayer-current-time';
+
 const player = new Player(iframe);
 
+// записуємо в localStorage текуще значення часу
+function onPlayTime({seconds}) {localStorage.setItem(localStorageTimeKey, seconds);
+    // console.log('evt.seconds==', evt.seconds);    
+};
+
+// получаемо із localStorage текуще значення
+const currentTimePKey = localStorage.getItem(localStorageTimeKey);
+// console.log('currentTimePKey=', currentTimePKey);
+
+// якщо є значення localStorage та != 0, встановлюємо це значення
+currentTimePKey ? player.setCurrentTime(currentTimePKey) : null;
+// if (currentTimePKey) {
+//     player.setCurrentTime(currentTimePKey);
+// }
+// else {
+//   currentTimePKey === null;
+// }
 player.on('timeupdate', throttle(onPlayTime, 1000), );
 
-function onPlayTime({seconds}) {
-    // console.log('evt.seconds==', evt.seconds);
-    localStorage.setItem(localStorageTimeKey, seconds);
-};
-player.setCurrentTime(localStorage.getItem(localStorageTimeKey));
 
 //  Для себе, тренувався
 player.on('play',onPlay);
@@ -21,6 +34,11 @@ function onPlay(evt) {
     console.log('Play clip', (evt.seconds/60).toFixed(2), "min");
 }
 
+
+
+
+// если есть сохраненная текущая позиция и она отлична от 0, устанавливаем на плеере сохраненную текущую позицию
+// currentTimeOfVideo ? player.setCurrentTime(currentTimeOfVideo) : null;
 
 
 
